c9193fa0dedd23dcf7fe712206efbe1b99103ddb
jassmanni <jassmanniq@gmail.com>
2021-03-12 21:11:58 -0400

    change validator on filed delivered to  IsBoolean  in orderDeliveredDto.

--- src/modules/order/dto/delivered.dto.ts
+++ src/modules/order/dto/delivered.dto.ts
@@ -1,9 +1,9 @@
-import { IsBooleanString, IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
 import { ExistsOnDatabase } from 'src/validations/exists-on-database';
 import { Order } from '../entities/order.entity';
 
 export class DeliveredDto {
-  @IsBooleanString()
+  @IsBoolean()
   delivered: boolean;
 
   @IsNotEmpty()
